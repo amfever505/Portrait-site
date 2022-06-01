c798bbb0414b2e8cb8284a53c5259ad2e4bb5075
amfever505 <amfeve505@gmail.com>
2022-06-01 19:05:08 +0900

    first commit

---
+++ MD31-課題01-02-陳姿穎/js/config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+  theme: {
+    maxWidth: {
+      'box-md': '1228px',
+      'box-lg': '1256px',
+    },
+    fontSize: {
+      sm: ['0.75rem', 1],
+      base: ['1rem', 1],
+      lg: ['1.875rem', 1],
+      xxl: ['10rem', 1],
+    },
+    extend: {
+      colors: {
+        main: '#E7D0D0',
+        txt: '#707070',
+        wt: '#fff',
+      },
+      margin: {
+        '5px': '5px',
+        15: '3.75rem',
+        68: '17rem',
+        69: '17.25rem',
+      },
+      spacing: {
+        '7/11': '63%',
+        '18/25': '72%',
+      },
+    },
+  },
+};
